d078a39a55ddbd71d8667e4291b2b484c17e36d5
Yacin Bahi <yacin@sensr.net>
2013-10-03 15:12:02 -0700

    first green test suite

--- test/main.js
+++ test/main.js
@@ -5,7 +5,7 @@ describe('sendsms', function() {
   describe('with no arguments', function() {
    it('returns an error', function() {
      var result = sendsms();
-     result.should.eql('Please provide a phone number, a phone carrier and a country');
+     result.should.eql('Please provide a phone number, a phone carrier');
    });
  });
 });
